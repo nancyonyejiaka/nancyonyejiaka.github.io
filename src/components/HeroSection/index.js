e474dff15f99f57334790e3bde0df4a935fd2e9f
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2023-11-14 13:07:11 -0500

    Reverted Hero IMG

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -13,7 +13,7 @@ import {
     TextLoop,
     Title
 } from './HeroStyle'
-import HeroImg from '../../images/knapkynIcon.png'
+import HeroImg from '../../images/HeroImage.jpg'
 import Typewriter from 'typewriter-effect';
 import {Bio} from '../../data/constants';
 
