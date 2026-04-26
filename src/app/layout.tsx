1009eaca25cc6a6b39c7278bcaa0c55d13c311d5
Nancy Onyejiaka <114533386+nancyonyejiaka@users.noreply.github.com>
2026-04-26 00:29:14 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
 				<link rel="icon" href="/favicon.png" type="image/png" />
 				<script
 					dangerouslySetInnerHTML={{
-						__html: `(function(){try{var t=localStorage.getItem("theme");if(t==="dark"||(!t&&window.matchMedia("(prefers-color-scheme:dark)").matches)){document.documentElement.classList.add("dark")}else{document.documentElement.classList.remove("dark")}}catch(e){}})()`,
+						__html: `(function(){try{var t=localStorage.getItem("theme");if(t==="dark"){document.documentElement.classList.add("dark")}}catch(e){}})()`,
 					}}
 				/>
 			</head>
